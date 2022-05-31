875d546eb6ff3343b0eb1d38e6a52dd3cc8eda53
Gabriel Silva <gabrielp14.silva@gmail.com>
2022-05-30 23:24:24 -0300

    add new script for about volume in database

---
+++ ViewVolumeBase.sql
@@ -0,0 +1,21 @@
+/*
+* Ver tamanho da base de dados
+*
+* A sentença abaixo irá mostrar o tamanho da base atualmente em Megas, onde deverá ser utilizado algum software para converter o valor
+*/
+
+EXEC sp_spaceused
+
+/*
+* Outra forma de consultar o tamanho da base de dados, é o script abaixo,
+* convertendo o valor da base em mega.
+*/
+
+DECLARE
+@BASEDEDADOS = 'Name_Database'
+GO
+
+USE @BASEDEDADOS
+GO
+SELECT DB_NAME() AS DatabaseName,(SUM(SIZE) / 1024) AS TamanhoMB
+FROM SYS.database_files
